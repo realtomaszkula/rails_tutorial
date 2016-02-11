c8d0281893184499d449c8daeac378fe00a1f01f
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-11 03:08:23 +0100

    Finish exercises

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
+  test "should get signup" do
+    get :signup
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
+
 
 end
