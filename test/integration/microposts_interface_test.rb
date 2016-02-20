af0027ed82561f73ed0c196872586fa8dc3a4712
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-20 01:44:22 +0100

    finish exe

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -6,10 +6,26 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
+  end
+
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: "" }
@@ -17,9 +33,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture}
     end
+    assert @user.microposts.first.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
